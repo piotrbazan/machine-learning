d73b309d6268ad508f9d347cadbf7829f7fc7e3b
Piotr Bazan <piotr.w.bazan@gmail.com>
2017-07-27 15:11:18 +0200

    Fix create features labels non beat params

--- projects/ecg/preprocess.py
+++ projects/ecg/preprocess.py
@@ -61,10 +61,11 @@ def data_stats(data):
         print('Normal beats: %d, arrhythmia: %d' % (N, A))
 
 
-def safe_random(i, margin, delta, size):
+def safe_random(i, non_beat_margin, delta, size):
+    low, high = non_beat_margin
     while True:
-        candidate = int(np.random.uniform(-180, 180))
-        if abs(i - candidate) > margin and i + candidate - delta > 0 and i + candidate + delta < size:
+        candidate = int(np.random.uniform(-high, high))
+        if abs(i - candidate) > low and i + candidate - delta > 0 and i + candidate + delta < size:
             return candidate
 
 
@@ -100,6 +101,8 @@ def create_features_labels(data, window_size=784, non_beats_per_beat = 9):
     :return: features, labels
     """
     x, y, delta = [], [], int(window_size / 2)
+    non_beat_margin = (6, 135)
+
     for d in data:
         ann, sig = d['annotations'], d['signals']
         sig_size = len(sig)
@@ -110,7 +113,7 @@ def create_features_labels(data, window_size=784, non_beats_per_beat = 9):
                     y.append(beat_type)
                     # add non beats
                     for j in range(non_beats_per_beat):
-                        c = safe_random(i, 10, delta, sig_size)
+                        c = safe_random(i, non_beat_margin, delta, sig_size)
                         x.append(np.array(sig['MLII'][i + c - delta:i + c + delta]))
                         y.append('NB')
 
